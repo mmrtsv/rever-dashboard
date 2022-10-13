2fe32bdec3c1dbde53a4a71ef3f646deb21b5bf0
Gerard Sole <58429274+Gerardss95@users.noreply.github.com>
2022-10-13 15:56:26 +0200

    First deploy Ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import './App.css';
+import ComingSoonAnimation from './Lottie/ComingSoon/ComingSoon';
+import LogoWide from './assets/images/icons/logoWide.svg';
 
 function App() {
-
   return (
     <div className='App'>
+      <img src={LogoWide} alt='Rever Logo' />
+      <ComingSoonAnimation />
       <h1>Coming soon...</h1>
     </div>
   );
---
+++ src/Lottie/ComingSoon/ComingSoon.tsx
@@ -0,0 +1,6 @@
+import Lottie from 'lottie-react';
+import ComingSoon from './ComingSoon.json';
+
+const ComingSoonAnimation = () => <Lottie animationData={ComingSoon} />;
+
+export default ComingSoonAnimation;
