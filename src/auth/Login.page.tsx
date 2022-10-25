f622b2e5662b89b1621f0264fa5e6ab8c3bf8377
JeanMonnV <jean.monnin.99@gmail.com>
2022-10-25 14:03:41 +0200

    FIX: some changes

--- src/auth/Login.page.tsx
+++ src/auth/Login.page.tsx
@@ -3,10 +3,12 @@ import { yupResolver } from '@hookform/resolvers/yup'
 import styled from 'styled-components'
 import Paper from '@mui/material/Paper'
 import LogoLogin from '../assets/images/icons/LogoLogin.svg'
+import LandingImage from '../assets/images/icons/LandingImage.svg'
 import TextField from '@mui/material/TextField'
 import * as yup from 'yup'
 import _ from 'lodash'
 import { Button } from '@mui/material'
+import Box from '@mui/material/Box'
 
 // Form validation
 const schema = yup.object().shape({
@@ -25,9 +27,8 @@ function LoginPage() {
     })
     const { errors, dirtyFields, isValid } = formState
 
-    let count = 1
     const onSubmit = () => {
-        count += 1
+        const count = 1
     }
 
     return (
@@ -43,7 +44,7 @@ function LoginPage() {
                     </UtilsContainer>
                     <h4 className="my-6">Sign in</h4>
                     <form
-                        className="flex flex-col justify-center w-full"
+                        className="flex w-full flex-col justify-center"
                         onSubmit={handleSubmit(onSubmit)}
                     >
                         <Controller
@@ -84,14 +85,18 @@ function LoginPage() {
                             aria-label="Sign in"
                             disabled={_.isEmpty(dirtyFields) || !isValid}
                             type="submit"
-                            size="large"
+                            size="medium"
+                            sx={{ borderRadius: 73 }}
                         >
                             Sign in
                         </Button>
                     </form>
                 </FormContainer>
             </LeftBox>
-            <div>{count}</div>
+
+            <RightBox>
+                <img src={LandingImage} alt="BackgroundImage" />
+            </RightBox>
         </MainDiv>
     )
 }
@@ -102,55 +107,60 @@ const MainDiv = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    flex: 1 1 0%;
-    min-width: '0px';
-    @media (min-width: '600px') {
-        flex-direction: row;
+    //flex: 1 1 0%;
+    //min-width: 0px;
+    @media (min-width: 600px) {
         justify-content: center;
+        background-color: aliceblue;
     }
-    @media (min-width: '900px') {
+    @media (min-width: 900px) {
+        flex-direction: row;
+        background-color: transparent;
         align-items: flex-start;
         justify-content: flex-start;
-    } ;
+    }
 `
 
 const LeftBox = styled(Paper)`
-    height: 100%;
+    height: 100vh;
     width: 100%;
     padding-top: 2rem;
     padding-bottom: 2rem;
     padding-left: 4rem;
     padding-right: 4rem;
-    border-right-width: '1px';
-    @media (min-width: '600px') {
+    border-right-width: 1px;
+    @media (min-width: 600px) {
+        margin-top: 6rem;
         width: auto;
         height: auto;
-        padding: 12rem;
+        padding: 6rem;
         border-radius: 1rem;
         box-shadow: 0 1px 3px 0 rgb(0 0 0 / 0.1),
             0 1px 2px -1px rgb(0 0 0 / 0.1);
     }
-    @media (min-width: '900px') {
+    @media (min-width: 900px) {
+        height: 100vh;
+        margin-top: 0;
         display: flex;
         align-items: center;
         justify-content: flex-end;
-        height: 100%;
         width: 50%;
-        padding: 16rem;
+        //padding: 16rem;
         border-radius: none;
         box-shadow: none;
     }
 `
 
 const FormContainer = styled.form`
+    height: 100%;
     width: 100%;
     max-width: 32rem;
     margin-right: auto;
     margin-left: auto;
-    @media (min-width: '600px') {
+    @media (min-width: 600px) {
         margin-right: 0%;
         margin-left: 0%;
-        width: 32rem;
+        width: 20rem;
     }
 `
 
@@ -158,4 +168,24 @@ const UtilsContainer = styled.div`
     display: flex;
     align-items: center;
     justify-content: space-between;
+`
+
+const RightBox = styled(Box)`
+    position: relative;
+    visibility: hidden;
+    flex: 1 1 auto;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    padding: 8rem;
+    overflow: hidden;
+    @media (min-width: 900px) {
+        visibility: visible;
+        display: flex;
+    }
+    @media (min-width: 1200px) {
+        visibility: visible;
+        padding-top: 11.2rem;
+        padding-bottom: 11.2rem;
+    }
 `
