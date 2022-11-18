0921978780a1dcf773ffbe4eedd37de467c7bfb9
JeanMonnV <jean.monnin.99@gmail.com>
2022-11-18 16:19:27 +0100

    FEAT: first commit to start building

---
+++ src/pages/OrdersByStatus.page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PageComponent from '../components/PageComponent'
+
+const OrdersByStatus = () => {
+    return (
+        <PageComponent>
+            <h1>OrdersByStatus</h1>
+        </PageComponent>
+    )
+}
+
+export default OrdersByStatus
