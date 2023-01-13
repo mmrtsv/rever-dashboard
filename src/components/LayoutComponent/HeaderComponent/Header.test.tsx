54dd388356ceed5b23d8b0e2a828eaeae24c9be5
JeanMonnV <jean.monnin.99@gmail.com>
2023-01-13 13:50:33 +0100

    FIX: more test

--- src/components/LayoutComponent/HeaderComponent/Header.test.tsx
+++ src/components/LayoutComponent/HeaderComponent/Header.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, screen, cleanup } from '@testing-library/react'
+import { render, screen, cleanup, fireEvent } from '@testing-library/react'
 import { afterEach, describe, expect, it } from 'vitest'
 import configureStore from 'redux-mock-store'
 import { BrowserRouter as Router } from 'react-router-dom'
@@ -12,8 +12,8 @@ import Header from './Header'
 describe('Header Component', () => {
     afterEach(cleanup)
 
-    it('should render the components: DrawerLogo - ReverLogo - LanguageSwitcher - UserMenu', () => {
-        const state = reduxState(true)
+    it('should render the components: BurgerMenu - ReverLogo - LanguageSwitcher - UserMenu', () => {
+        const state = reduxState(false)
         const mockStore = configureStore()
         const store = mockStore(state)
 
@@ -34,26 +34,53 @@ describe('Header Component', () => {
         screen.getByTestId('UserMenu')
     })
 
-    // it('should no longer display DrawerLogo when open', () => {
-    //     render(
-    //         <Router>
-    //             <Provider store={store}>
-    //                 <I18nextProvider i18n={i18n}>
-    //                     <ThemeProvider>
-    //                         <Header />
-    //                     </ThemeProvider>
-    //                 </I18nextProvider>
-    //             </Provider>
-    //         </Router>
-    //     )
-    //     expect(screen.queryByTestId('DrawerOutLogo')).toBeNull()
-    // })
+    it('should not display the Buger Menu when the drawer is open', () => {
+        const state = reduxState(true)
+        const mockStore = configureStore()
+        const store = mockStore(state)
+        render(
+            <Router>
+                <Provider store={store}>
+                    <I18nextProvider i18n={i18n}>
+                        <ThemeProvider>
+                            <Header />
+                        </ThemeProvider>
+                    </I18nextProvider>
+                </Provider>
+            </Router>
+        )
+        expect(screen.queryByTestId('BurgerMenuOutsideIcon')).toBeNull()
+        screen.getByAltText('logo')
+        screen.getByTestId('LanguageSwitcher')
+        screen.getByTestId('UserMenu')
+    })
+
+    it('should dispatch when clicking the Burger Menu', () => {
+        const state = reduxState(false)
+        const mockStore = configureStore()
+        const store = mockStore(state)
+
+        render(
+            <Router>
+                <Provider store={store}>
+                    <I18nextProvider i18n={i18n}>
+                        <ThemeProvider>
+                            <Header />
+                        </ThemeProvider>
+                    </I18nextProvider>
+                </Provider>
+            </Router>
+        )
+        fireEvent.click(screen.getByTestId('BurgerMenuOutsideIcon'))
+        const actions = store.getActions()
+        expect(actions.length).toBe(1)
+    })
 })
 
 function reduxState(open: boolean) {
     return {
         appState: {
-            isSideBarOpen: open
+            isSidebarOpen: open
         }
     }
 }
--- src/components/LayoutComponent/HeaderComponent/UserMenu/UserMenu.test.tsx
+++ src/components/LayoutComponent/HeaderComponent/UserMenu/UserMenu.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, screen, cleanup } from '@testing-library/react'
+import { render, screen, cleanup, fireEvent } from '@testing-library/react'
 import { afterEach, describe, it, vi, expect } from 'vitest'
 import { UserMenu, userOptions } from './UserMenu'
 import { Provider } from 'react-redux'
